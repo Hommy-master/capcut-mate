9d9774c9808a61b910cd123a01a2bffbb3192ab0
Hommy <16620803786@163.com>
2025-09-07 15:18:17 +0800

    更新默认配置。

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 
 # 确保路径使用正确的操作系统分隔符
 DRAFT_DIR = os.path.join(os.path.dirname(__file__), "output", "draft")
-DRAFT_URL = "https://capcut.jcaigc.cn/openapi/v1/get_draft"
+DRAFT_URL = os.getenv("DRAFT_URL", "https://cm.jcaigc.cn/openapi/v1/get_draft")
