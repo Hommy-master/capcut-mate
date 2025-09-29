1f28cf76bcd5af98c2bbd5422404a93f2e714cb8
Hommy <16620803786@163.com>
2025-09-29 23:44:22 +0800

    更新配置。

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DRAFT_DIR = os.path.join(os.path.dirname(__file__), "output", "draft")
 TEMP_DIR = os.path.join(os.path.dirname(__file__), "temp")
 
 # 剪映草稿的下载路径
-DRAFT_URL = os.getenv("DRAFT_URL", "https://capcut-mate.jcaigc.cn/openapi/v1/get_draft")
+DRAFT_URL = os.getenv("DRAFT_URL", "https://capcut-mate.jcaigc.cn/openapi/capcut-mate/v1/get_draft")
 
 # 将容器内的文件路径转成一个下载路径，执行替换操作，即将/app/ -> https://capcut-mate.jcaigc.cn/
 DOWNLOAD_URL = os.getenv("DOWNLOAD_URL", "https://capcut-mate.jcaigc.cn/")
