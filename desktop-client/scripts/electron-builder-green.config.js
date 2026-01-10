4fbad6243be7d626b7d73cdb4a6f0f9b577845e2
Hommy <16620803786@163.com>
2026-01-10 11:45:49 +0800

    上传漏掉的问题。

---
+++ desktop-client/scripts/electron-builder-green.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  appId: "com.gogoshine.capcutmate",
+  productName: "CapCut-Mate",
+  directories: {
+    output: "dist"
+  },
+  files: [
+    "!node_modules/**/*",
+    "!web/node_modules/**/*",
+    "!web/src/**/*",
+    "!web/public/**/*",
+    "!scripts/**/*",
+    "!*.yml",
+    "!*.yaml",
+    "!*.lock",
+    "!README.md",
+    "!Dockerfile",
+    "!docker-compose.yaml",
+    "node_modules/**/*",
+    "web/dist/**/*",
+    "*.js",
+    "*.json",
+    "assets/**/*"
+  ],
+  extraResources: [
+    {
+      from: "assets/",
+      to: "assets/"
+    }
+  ]
+};
