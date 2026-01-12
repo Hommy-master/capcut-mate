f4b26820dcc7e2a76d0cd79a95091a260f9155d0
Hommy <16620803786@163.com>
2026-01-12 13:45:07 +0800

    解决窗口无法显示的问题。

--- desktop-client/main.js
+++ desktop-client/main.js
@@ -25,11 +25,6 @@ function createWindow() {
     iconPath = path.join(__dirname, './assets/icons/logo.png');
   }
 
-  // 在 macOS 上设置应用图标
-  if (process.platform === 'darwin') {
-    app.setIcon(iconPath); // 设置应用程序图标
-  }
-
   mainWindow = new BrowserWindow({
     width: 1366,
     height: 868,
@@ -44,6 +39,11 @@ function createWindow() {
       hardwareAcceleration: true // 启用硬件加速
     }
   });
+
+  // 在 macOS 上设置 Dock 图标
+  if (process.platform === 'darwin') {
+    app.dock.setIcon(path.join(__dirname, './assets/icons/logo.icns'));
+  }
 
   // 判断是否为开发模式
   const isDev = process.env.NODE_ENV === 'development' || !app.isPackaged;
