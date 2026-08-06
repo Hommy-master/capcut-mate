5958dd811eabaa0d8c5b541376171cd91e2a4a39
Hommy <16620803786@163.com>
2026-08-06 11:20:12 +0800

    更新注释。

--- src/service/add_filters.py
+++ src/service/add_filters.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Hommy <taohongmin@sina.cn>.
+# Copyright 2026 Hommy <taohongmin@sina.cn>.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
